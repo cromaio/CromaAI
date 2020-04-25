92677ec07adda70027c34778a2fae928558fb149
Julian Ganzabal <jganzabal@gmail.com>
2020-04-25 18:52:15 -0300

    nohup gitignore

---
+++ scripts/install.sh
@@ -0,0 +1,34 @@
+# Bajada de repositorio git
+git clone git@github.com:cromaio/CromaAI.git 
+cd CromaAI
+
+# Creación de entorno conda
+conda env create -f environment.yml 
+
+conda activate cromaAI
+
+# Base de datos
+mkdir data
+cd data
+mkdir db
+cd ..
+nohup mongod --port 27018 --dbpath ./data/db/ &
+
+# Bajar articulos de ejemplo
+cp ./CromaAI/config.py.sample ./CromaAI/config.py
+python ./CromaAI/fetch_articles.py
+
+# Bajado de modelos
+cd CromaAI/
+mkdir models
+cd models
+wget https://croma.ai/models/models_demo.zip
+unzip models_demo.zip
+cd ..
+cp config_train.py.sample config_train.py
+
+# Entrenamiento
+python train_models.py
+
+# Correr API
+python FlaskAPI.py -p 5000 -h localhost
